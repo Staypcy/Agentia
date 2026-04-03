8e5c906b237aabd4d15c5f7a2c2fb3ce5e1f56a3
stay <2174738782@qq.com>
2026-04-03 20:08:41 +0800

    delete build

--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Agentia_FILE D:/QtProject/Agentia/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/Agentia.exe)
-set(__QT_DEPLOY_TARGET_Agentia_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Agentia_RUNTIME_DLLS D:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
